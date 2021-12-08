e121a12ae573f20c8ec54964e631e87688c0edcd
Arison13 <arisonarias@gmail.com>
2021-12-08 04:10:06 -0500

    work in progress

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+
+const cars = [ 
+    {
+        vin:"11111111111",
+        make: "toyota",
+        model: "prius",
+        mileage: 48000,
+        title: "clean",
+        transmission: "automatic"
+    },
+    {
+        vin:"11111111112",
+        make: "toyota",
+        model: "corolla",
+        mileage: 58000,
+        title: "salvage",
+    },
+    {
+        vin:"11111111113",
+        make: "ford",
+        model: "focus",
+        mileage: 68000
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
