7e169804a948ddb916bb299c6dec53d8fdb61c0c
Arison13 <arisonarias@gmail.com>
2021-12-08 05:20:21 -0500

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,29 @@
 // DO YOUR MAGIC
+const express = require('express');
+const Car = require ('./cars-model')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try{
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err){
+        next(err)
+    }
+    res.json('getting all cars')
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const car = await Car.getById(req.params.id)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', (req, res, next) => {
+    res.json('posting a new car')
+})
+
+module.exports = router
