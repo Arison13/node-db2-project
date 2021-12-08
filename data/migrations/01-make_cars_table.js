df9e247fc4f8830fd4b3c908a049f7ff242a2358
Arison13 <arisonarias@gmail.com>
2021-12-08 04:49:01 -0500

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -8,7 +8,7 @@ exports.up = function (knex) {
     tbl.string('make', 128).notNullable()
     tbl.string('model', 128).notNullable()
 
-    tbl.numeric('mileage').unsigned().notNullable()
+    tbl.integer('mileage').unsigned().notNullable()
 
     tbl.string('title', 128)
     tbl.string('transmission', 128)
